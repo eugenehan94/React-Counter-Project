821dee1cc8ca016c3963ab9131b76cc951870a41
eugenehan94 <eugene.han94@gmail.com>
2021-05-27 21:16:36 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+//Create a counter app - increase, decrease and reset
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseByOne = () => {
+    setCount((prevState) => {
+      return prevState + 1;
+    });
+  };
+  const increaseByTen = () => {
+    setCount((prevState) => {
+      return prevState + 10;
+    });
+  };
+  const increaseByHundred = () => {
+    setCount((prevState) => {
+      return prevState + 100;
+    });
+  };
+  const reset = () => {
+    setCount((prevState) => {
+      return 0;
+    });
+  };
+
+  const decreaseByOne = () => {
+    setCount((prevState) => {
+      return prevState - 1;
+    });
+  };
+  const decreaseByTen = () => {
+    setCount((prevState) => {
+      return prevState - 10;
+    });
+  };
+  const decreaseByHundred = () => {
+    setCount((prevState) => {
+      return prevState - 100;
+    });
+  };
+
+  const randomPositive = () => {
+    setCount((prevState) => {
+      return Math.floor(Math.random() * (99999 - 0) + 0);
+    });
+  };
+
+  const randomNegative = () => {
+    setCount((prevState) => {
+      return Math.floor(Math.random() * (-99999 - 0) + 0);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Complex Counter</h1>
+
+      <h1>{count}</h1>
+
+      <div className="buttons">
+        <button onClick={increaseByOne}>Increase 1</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={decreaseByOne}>Decrease 1</button>
+        <button onClick={randomPositive}>Random + Number</button>
+        <button onClick={randomNegative}>Random - Number</button>
+
+        <button onClick={increaseByTen}>Increase 10</button>
+        <button onClick={decreaseByTen}>Decrease 10</button>
+
+        <button onClick={increaseByHundred}>Increase 100</button>
+        <button onClick={decreaseByHundred}>Decrease 100</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
