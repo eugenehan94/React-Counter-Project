42de495d2955204903e38c6675b46f2be85193c0
eugenehan94 <eugene.han94@gmail.com>
2021-11-10 11:46:18 -0500

    Added test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,120 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-it("should be header complex counter", () => {
-  render(<App />);
-  const headerElement = screen.getByText(/complex counter/i);
-  expect(headerElement).toBeInTheDocument;
-});
-
-it("should render a heading role", () => {
-  render(<App />);
-  const headingElement = screen.getByRole("heading", {
-    name: "Complex Counter",
+describe("header test", () => {
+  it("primary header present - getByRole", () => {
+    render(<App />);
+    // The following is a screen debug which acts like console.log
+    // screen.debug(screen.getByRole("heading", { name: "Complex Counter" }));
+    expect(screen.getByRole("heading", { name: "Complex Counter" }))
+      .toBeInTheDocument;
   });
-  expect(headingElement).toBeInTheDocument;
+  it("primary header present - getByText", () => {
+    render(<App />);
+    // screen.debug(screen.getByText(/complex counter/i));
+    expect(screen.getByText(/complex counter/i)).toBeVisible;
+  });
+  it("primary header present - getByTestid - last resort query", () => {
+    render(<App />);
+    const headerElement = screen.getByTestId("header-1");
+    // screen.debug(screen.getByTestId("header-1"));
+    expect(headerElement).toBeInTheDocument;
+  });
 });
 
-it("should render if asynchronous", async () => {
-  render(<App />);
-  const headingElement = await screen.findByText(/complex counter/i);
-  expect(headingElement).toBeInTheDocument;
+describe("counter header", () => {
+  it("if it is present - getByRole", () => {
+    render(<App />);
+    // screen.debug(screen.getByTitle("countTitle"));
+    expect(screen.getByTitle("countTitle")).toBeInTheDocument;
+  });
 });
 
-it("query not to be in app", async () => {
-  render(<App />);
-  const headingElement = screen.queryByText(/dogs/i);
-  expect(headingElement).not.toBeInTheDocument;
+describe("get list of ", () => {
+  it("all headers", () => {
+    render(<App />);
+    // screen.debug(screen.getAllByRole("heading"));
+    expect(screen.getAllByRole("heading")).toBeInTheDocument;
+  });
+
+  it("buttons", () => {
+    render(<App />);
+    // screen.debug(screen.getAllByRole("button"));
+    expect(screen.getAllByRole("button")).toBeVisible;
+  });
 });
 
-it("should render multiple heading roles", () => {
-  render(<App />);
-  const headingElements = screen.queryAllByRole("heading");
-  expect(headingElements.length).toBe(2);
+describe("Integrations tests", () => {
+  it("check that the count is zero initially", () => {
+    render(<App />);
+    expect(screen.getByTitle("countTitle").textContent).toBe("0");
+  });
+
+  it("click increase 1 button, counter does up 1", () => {
+    render(<App />);
+    // screen.debug(screen.getByRole("button", { name: "Increase 1" }));
+    const targetButton = screen.getByRole("button", { name: "Increase 1" });
+    fireEvent.click(targetButton);
+    const targetCount = screen.getByTitle("countTitle");
+    expect(targetCount.textContent).toBe("1");
+  });
+
+  it("click decrease 1 button, counter goes down 1", () => {
+    render(<App />);
+    const targetButton = screen.getByRole("button", { name: "Decrease 1" });
+    fireEvent.click(targetButton);
+    expect(screen.getByTitle("countTitle").textContent).toBe("-1");
+  });
+
+  it("reset after increase 1", () => {
+    render(<App />);
+    // screen.debug(screen.getByTitle("countTitle"));
+    fireEvent.click(screen.getByRole("button", { name: "Increase 1" }));
+    expect(screen.getByTitle("countTitle").textContent).toBe("1");
+    fireEvent.click(screen.getByRole("button", { name: "Reset" }));
+    expect(screen.getByTitle("countTitle").textContent).toBe("0");
+  });
+  it("is zero falsey", () => {
+    render(<App />);
+    expect(screen.getByTitle("countTitle").textContent).toBeFalsy;
+  });
+  it("random positive number", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: "Random + Number" }));
+    const count = parseInt(screen.getByTitle("countTitle").textContent);
+    expect(count).toBeGreaterThan(0);
+  });
+  it("random negative number", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: "Random - Number" }));
+    expect(parseInt(screen.getByTitle("countTitle").textContent)).toBeLessThan(
+      0
+    );
+  });
+  it("increase by 10", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: "Increase 10" }));
+    expect(screen.getByTitle("countTitle").textContent).toBe("10");
+  });
+  it("decrease by 10", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: "Decrease 10" }));
+    expect(screen.getByTitle("countTitle").textContent).toBe("-10");
+  });
+  it("increase by 100", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: "Increase 100" }));
+    expect(screen.getByTitle("countTitle").textContent).toBe("100");
+  });
+  it("decrease by 100", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: "Decrease 100" }));
+    expect(screen.getByTitle("countTitle").textContent).toBe("-100");
+  });
+  it("decrease by 10, then reset to zero", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: "Decrease 100" }));
+    fireEvent.click(screen.getByRole("button", { name: "Reset" }));
+    expect(screen.getByTitle("countTitle").textContent).toBe("0");
+  });
 });
